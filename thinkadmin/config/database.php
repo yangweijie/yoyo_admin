194df06c344ddb4f8a5a7f83a629fb3e148d1902
yangweijie <917647288@qq.com>
2023-06-28 08:36:26 +0800

    添加yoyo配置 将queue 表 备份下来

--- thinkadmin/config/database.php
+++ thinkadmin/config/database.php
@@ -25,6 +25,42 @@ return [
     'datetime_format' => 'Y-m-d H:i:s',
     // 数据库连接配置信息
     'connections'     => [
+        'yoyo'  => [
+            // 数据库类型
+            'type'            => 'mysql',
+            // 服务器地址
+            'hostname'        => '127.0.0.1',
+            // 数据库名
+            'database'        => 'test',
+            // 用户名
+            'username'        => 'root',
+            // 密码
+            'password'        => 'root',
+            // 端口
+            'hostport'        => '3306',
+            // 数据库连接参数
+            'params'          => [],
+            // 数据库编码默认采用 utf8
+            'charset'         => 'utf8',
+            // 数据库表前缀
+            'prefix'          => '',
+            // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
+            'deploy'          => 0,
+            // 数据库读写是否分离 主从式有效
+            'rw_separate'     => false,
+            // 读写分离后 主服务器数量
+            'master_num'      => 1,
+            // 指定从服务器序号
+            'slave_no'        => '',
+            // 检查字段是否存在
+            'fields_strict'   => true,
+            // 是否需要断线重连
+            'break_reconnect' => false,
+            // 监听SQL执行日志
+            'trigger_sql'     => true,
+            // 开启字段类型缓存
+            'fields_cache'    => isOnline(),
+        ],
         'mysql'  => [
             // 数据库类型
             'type'            => 'mysql',
