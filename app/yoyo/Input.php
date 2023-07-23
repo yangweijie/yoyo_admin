3a88d1cab0356386b95f8d6522375920943ad1c0
yangweijie <917647288@qq.com>
2023-07-23 22:09:06 +0800

    升级tailwind composer 兼容8.0

--- app/yoyo/Input.php
+++ app/yoyo/Input.php
@@ -3,6 +3,7 @@
 namespace app\yoyo;
 
 use Clickfwd\Yoyo\Component;
+use Clickfwd\Yoyo\Exceptions\MissingComponentTemplate;
 
 class Input extends Component
 {
@@ -12,11 +13,14 @@ class Input extends Component
 	public $disabled = false;
 	public $value    = '';
 	public $inline   = false;
-	public $size     = 'text-base';
+	public $size     = '1.6'; // lg 2.15 default 1.6
 	public $label    = 'input';
 	public $required = false;
 	public $readonly = false;
 
+	/**
+	 * @throws MissingComponentTemplate
+	 */
 	public function render()
 	{
 		return (String) $this->view('input', [
